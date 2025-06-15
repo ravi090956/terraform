46bab99d6f995328907a2d447dcd615b27c9577c
ravi kumarc <ravi.kumar-c@capgemini.com>
2025-06-16 00:37:12 +0530

    practice

--- rds.tf
+++ rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "default" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t3.micro"
   db_name              = "mydb"
   username             = "foo"
   password             = "${file("../rds_pass.txt")}"
